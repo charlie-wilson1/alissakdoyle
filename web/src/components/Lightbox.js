992f0696a2259e4ce169d32df0516e1c495b6347
johnxferraro <johnxferraro@gmail.com>
2019-11-08 19:16:17 -0800

    multiple gallery lightbox images

--- web/src/components/Lightbox.js
+++ web/src/components/Lightbox.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { css } from '@emotion/core';
 import Img from "gatsby-image";
 
@@ -10,29 +10,37 @@ const lightboxShown = css`
   width: 100vw;
   z-index: 10;
   background-color: rgb(0, 0, 0, 0.9);
-  transition: background-color 0.3s;
+  transition: background-color 0.2s;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
 const lightboxHidden = css`
-  position: relative;
+  position: absolute;
   height: 100%;
   width: 100%;
-  transition: background-color 0.3s;
+  z-index: 1;
 `;
 
-const Lightbox = (props) => {
+const Lightbox = ({imageAssets}) => {
   const [lightbox, toggleLightbox] = useState(false);
-  const lightboxImage = props.image || null;
+  const [windowWidth, setwindowWidth] = useState(0);
+
+  useEffect(() => {
+    setwindowWidth(window.innerWidth);
+  }, [])
+
   return (
-    <div css={lightbox ? lightboxShown : lightboxHidden} {...props} onClick={(e) => {toggleLightbox(!lightbox); console.log(lightbox, e.currentTarget)}}>
-      {lightboxShown && (
-        <>
-          {!lightbox && props.children}
-          <Img fixed={props.image} id={lightboxImage}></Img>
-        </>
+    <div css={lightbox ? lightboxShown : lightboxHidden} onClick={ (e) => {toggleLightbox(!lightbox)} } >
+      {(lightbox && windowWidth >= 700) && (
+        <Img fixed={imageAssets.large} key={imageAssets.large.src}></Img>
+      )}
+      {(lightbox && (windowWidth >= 500 && windowWidth < 700)) && (
+        <Img fixed={imageAssets.tablet} key={imageAssets.tablet.src}></Img>
+      )}
+      {(lightbox && windowWidth < 500) && (
+        <Img fixed={imageAssets.mobile} key={imageAssets.mobile.src}></Img>
       )}
     </div>
   );
