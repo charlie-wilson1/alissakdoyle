97d6371a0cd55886447f4f5f0d98bfe9ed84ef3b
johnxferraro <johnxferraro@gmail.com>
2019-11-10 11:03:12 -0800

    favi and db to prod

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
       resolve: 'gatsby-source-sanity',
       options: {
         projectId: '8n7w4ele',
-        dataset: 'dev',
+        dataset: 'production',
         // a token with read permissions is required
         // if you have a private dataset
         token: process.env.MY_SANITY_TOKEN,
@@ -42,13 +42,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `alissakdoyle`,
+        short_name: `alissakdoyle`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/a-favi.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
